8e80ba7a4a211542c73bff19400051cbc0b5832a
Ankit pandey <89537954+ankitpandey21k@users.noreply.github.com>
2023-04-01 02:45:59 +0530

    Update Strair_case.java

--- DP/Strair_case.java
+++ DP/Strair_case.java
@@ -1,3 +1,9 @@
+//A child runs up a staircase with 'n' steps and can hop either 1 step, 2 steps or 3 steps at a time. Implement a method to count and return all possible ways in which the child can run-up to the stairs.
+//Input format :
+//The first and the only line of input contains an integer value, 'n', denoting the total number of steps.
+//Output format :
+//Print the total possible number of ways.
+
 package DP;
 
 import java.util.Scanner;
